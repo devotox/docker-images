79fda1f659623d6b77bdec71248cedff58c1f992
Devonte <devo.tox.89@gmail.com>
2016-06-29 08:50:40 +0100

    update docker files

--- jenkins/utils/jenkins-clean.sh
+++ jenkins/utils/jenkins-clean.sh
@@ -4,10 +4,12 @@ echo
 echo "================================= Clean Jenkins ===================================="
 echo
 
+application="*"
+
 # Remove branch logs
 echo
 echo "Remove Jenkins Logs..."
-rm -rf /var/jenkins_home/workspace/Doctify/platform/*
-rm -rf /var/jenkins_home/jobs/Doctify/jobs/platform/branches/*
+rm -rf /var/jenkins_home/workspace/$application/platform/*
+rm -rf /var/jenkins_home/jobs/$application/jobs/platform/branches/*
 
 echo
