5151af3394e692a61f2804f6c7794a235283700e
Devonte <devo.tox.89@gmail.com>
2017-03-04 20:50:26 +0000

    updates

--- registrator/entrypoint.sh
+++ registrator/entrypoint.sh
@@ -2,6 +2,7 @@
 
 if [ -z "$DOCKER_HOST_IP" ]; then
 	export DOCKER_HOST_IP=$(curl --retry 5 --connect-timeout 3 -s 169.254.169.254/latest/meta-data/local-ipv4)
+	export DOCKER_HOST_NAME=$(curl --retry 5 --connect-timeout 3 -s 169.254.169.254/latest/meta-data/local-hostname)
 fi
 
 /bin/registrator "-ip=$DOCKER_HOST_IP" "$@"
